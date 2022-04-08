7e891fb25bf877beafde2202976171cf30ba3ad0
Kendrick Shaw <kms15@case.edu>
2022-04-08 22:30:00 +0000

    add doctests, test examples, and a testwatch target

--- {{cookiecutter.project_slug}}/test/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/test/test_{{cookiecutter.project_slug}}.py
@@ -1,4 +1,15 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright {% now 'local', '%Y' %}, {{ cookiecutter.full_name }}
+
 import {{cookiecutter.project_slug}}
+import pytest
+
 
 def test_foo():
-    assert {{cookiecutter.project_slug}}.foo() == True
+    # here's an example of a simple test
+    assert {{cookiecutter.project_slug}}.foo(3) == 5
+
+    # here's how to test that an exception is raised:
+    with pytest.raises(TypeError) as e:
+        {{cookiecutter.project_slug}}.foo("b")
+    assert str(e.value) == "unsupported operand type(s) for +: 'int' and 'str'"
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,5 +1,25 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright {% now 'local', '%Y' %}, {{ cookiecutter.full_name }}
 
-def foo():
-    return True
+
+def foo(x):
+    """ an example with tests in a separate file """
+    return 2 + x
+
+
+def bar(y):
+    """an example using a doctest
+    (note that you could have both external tests and doctests for the same
+    function)
+
+    Here's a simple test
+    >>> bar("b")
+    'ab'
+
+    Here's an example of testing that an exception is thrown:
+    >>> bar(3)
+    Traceback (most recent call last):
+        ...
+    TypeError: can only concatenate str (not "int") to str
+    """
+    return "a" + y
